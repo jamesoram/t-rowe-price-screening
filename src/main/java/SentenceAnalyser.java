7a6c8ffe348e3da1ce068fdf5140fa7a4c77bdac
James Oram <herespam1@hotmail.com>
2020-03-18 22:11:01 +0000

    add more tests and complete implementation

--- src/main/java/SentenceAnalyser.java
+++ src/main/java/SentenceAnalyser.java
@@ -3,18 +3,14 @@ import java.util.Arrays;
 public class SentenceAnalyser {
 
     public SentenceInfo getSentenceInfo(String sentence) {
-        SentenceInfo longestWordInfo = getLongestWordAndLength(sentence);
-        SentenceInfo shortestWordInfo = getShortestWordAndLength(sentence);
-        return new SentenceInfo(longestWordInfo.getLongestWord(), shortestWordInfo.getShortestWord());
-    }
-
-    public SentenceInfo getLongestWordAndLength(String sentence) {
         // not handling contractions - they are considered words
         // numbers are not considered words
+        // non-alphabet characters are stripped out
         // do we need to handle case?
         // in case of multiple words of the same length we take the last occurrence
-        sentence.replaceAll("[^a-zA-Z ]", "");
-        String[] words = sentence.split(" ");
+        // only works with Latin alphabet
+        // an assumption is made that we would like to return an empty string and a zero instead of throwing an exception when receiving an empty sentence
+        String[] words = sentence.replaceAll("[^a-zA-Z ]", "").split(" ");
         Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
         if (words.length > 0) {
             return new SentenceInfo(words[words.length - 1], words[0]);
@@ -23,7 +19,11 @@ public class SentenceAnalyser {
         }
     }
 
+    public SentenceInfo getLongestWordAndLength(String sentence) {
+        return getSentenceInfo(sentence);
+    }
+
     public SentenceInfo getShortestWordAndLength(String sentence) {
-        return getLongestWordAndLength(sentence);
+        return getSentenceInfo(sentence);
     }
 }
--- src/test/java/SentenceAnalyserTest.java
+++ src/test/java/SentenceAnalyserTest.java
@@ -14,8 +14,13 @@ public class SentenceAnalyserTest {
     public static Collection<Object[]> parameters() {
         return Arrays.asList(
                 new Object[][] {
-                        {"The cow jumped over the moon.", "jumped", 6},
-                        {"Lorem ipsum", "ipsum", 5}
+                        {"The cow jumped over the moon.", "jumped", 6, "The", 3},
+                        {"Lorem ipsum", "ipsum", 5, "Lorem", 5},
+                        {"A career at T. Rowe Price says you want to contribute", "contribute", 10, "A", 1},
+                        {"", "", 0, "", 0},
+                        {"31337, ", "", 0, "", 0},
+                        {"It's a great sentence", "sentence", 8, "a", 1},
+                        {"漢語 汉语 汉字/漢字", "", 0, "", 0}
                 }
         );
     }
@@ -23,20 +28,17 @@ public class SentenceAnalyserTest {
     private String sentence;
     private String longestWord;
     private int longestWordLength;
+    private String shortestWord;
+    private int shortestWordLength;
     private SentenceAnalyser sentenceAnalyser = new SentenceAnalyser();
 
-    public SentenceAnalyserTest(String sentence, String longestWord, int longestWordLength) {
+    public SentenceAnalyserTest(String sentence, String longestWord, int longestWordLength,
+                                String shortestWord, int shortestWordLength) {
         this.sentence = sentence;
         this.longestWord = longestWord;
         this.longestWordLength = longestWordLength;
-    }
-
-    // an assumption is made that we would like to return an empty string and a zero instead of throwing an exception
-    @Test
-    public void testEmptySentence() {
-        SentenceInfo sentenceInfo = sentenceAnalyser.getLongestWordAndLength("");
-        assertEquals("", sentenceInfo.getLongestWord());
-        assertEquals(0, sentenceInfo.getLongestWordLength());
+        this.shortestWord = shortestWord;
+        this.shortestWordLength = shortestWordLength;
     }
 
     @Test
@@ -44,5 +46,12 @@ public class SentenceAnalyserTest {
         SentenceInfo sentenceInfo = sentenceAnalyser.getLongestWordAndLength(sentence);
         assertEquals(longestWord, sentenceInfo.getLongestWord());
         assertEquals(longestWordLength, sentenceInfo.getLongestWordLength());
+    }
+
+    @Test
+    public void testShortestWordAndLength() {
+        SentenceInfo sentenceInfo = sentenceAnalyser.getShortestWordAndLength(sentence);
+        assertEquals(shortestWord, sentenceInfo.getShortestWord());
+        assertEquals(shortestWordLength, sentenceInfo.getShortestWordLength());
     }
 }
